3e3cd32e999c271a1aa99e0a87e938e95b141cc9
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-10-17 17:10:59 +0200

    Added Validation To Register Screen

--- src/Screens/RegisterScreen/index.js
+++ src/Screens/RegisterScreen/index.js
@@ -7,7 +7,23 @@ import {AppButton} from '../../Components/AppButton';
 import {placeholder, Strings} from '../../Ulitis/Strings';
 import styles from './style';
 
+const ValidateName = (NameText) => {
+  return /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/.test(
+    NameText,
+  );
+};
+const ValidateMail = (MailText) => {
+  return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(MailText);
+};
+const ValidatePass = (PassText) => {
+  return /^[a-zA-Z0-9.-]{6,}$/.test(PassText);
+};
+
 export const RegisterScreen = (props) => {
+  const [nameVal, ChangedNameVal] = React.useState('');
+  const [mailVal, ChangedMailVal] = React.useState('');
+  const [PassVal, ChangedPasslVal] = React.useState('');
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.Header}>
@@ -28,25 +44,44 @@ export const RegisterScreen = (props) => {
           placeholder={placeholder.ConactUsName}
           renderIcon={<Icon name={'person-outline'} style={styles.InputIcon} />}
           marginTop={50}
+          onChangeText={ChangedNameVal}
         />
         <Input
           underlined
           placeholder={placeholder.ConactUsMail}
           renderIcon={<Icon name={'mail-outline'} style={styles.InputIcon} />}
           marginTop={50}
+          onChangeText={ChangedMailVal}
         />
         <Input
           underlined
           placeholder={placeholder.UpdateAccountPass}
           renderIcon={<Icon name={'key-outline'} style={styles.InputIcon} />}
           marginTop={50}
+          autoCorrect={false}
+          autoCompleteType={'password'}
+          onChangeText={ChangedPasslVal}
+          secureTextEntry={true}
         />
         <View style={{marginTop: 45}}>
           <AppButton
             title={placeholder.RegisterButton}
             WrapperStyle={{colore: Colors.yellow, paddingVertical: 10}}
             titleStyle={{fontSize: 17, paddingVertical: 7}}
-            onPress = {() => {alert('reristerd')}}
+            onPress={() => {
+              if (
+                !ValidatePass(PassVal) ||
+                !ValidateName(nameVal) ||
+                !ValidateMail(mailVal)
+              ) {
+                alert('wrong ');
+                return;
+              }
+              {
+                alert('you enterd ' + nameVal + nameVal + mailVal);
+                return;
+              }
+            }}
           />
         </View>
         <View
