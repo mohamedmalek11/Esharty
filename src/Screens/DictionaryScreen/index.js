fbee714eb9b937352a8b4411a467793e3cab8d91
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-10-29 03:03:35 +0200

    Added Dictionary Component To Dictionary Screen

--- src/Screens/DictionaryScreen/index.js
+++ src/Screens/DictionaryScreen/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import {View, Text, ScrollView, SafeAreaView, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {dummyDictionary} from '../../Ulitis/DummyData';
-import {Strings} from '../../Ulitis/Strings'
-import {Dictionary} from '../../Components/Dictionary';
+import {Strings} from '../../Ulitis/Strings';
+import {Card} from '../../Components/Card';
 import styles from './style';
+
+const Dictionary = (props) => {
+  const {Dictionary} = props;
+
+  return (
+    <View>
+      <Card style={styles.Container}>
+        <View style={styles.Wrapper}>
+          <View style={styles.ImageWrapper}>
+            <Image
+              source={require('../../Assets/Images/Dictionary.png')}
+              style={styles.Image}
+            />
+          </View>
+          <View style={styles.ContentWrapper}>
+            <View style={styles.TextWrapper}>
+              <Text style={styles.MainTitle}>{Dictionary.MainTitle}</Text>
+              <Text style={styles.SecTitle}>{Dictionary.SecTite}</Text>
+            </View>
+            <View style={styles.IconWrapper}>
+              <Card>
+                <Icon name="eye" style={styles.Icon} />
+              </Card>
+            </View>
+          </View>
+          <View style={styles.DescriptionWrapper}>
+            <Text style={styles.Description}>{Dictionary.Description}</Text>
+          </View>
+        </View>
+      </Card>
+    </View>
+  );
+};
 
 export const DictionaryScreen = (props) => {
   const renderDictionary = ({item}) => {
--- src/Screens/DictionaryScreen/style.js
+++ src/Screens/DictionaryScreen/style.js
@@ -2,14 +2,80 @@ import {ScaledSheet} from 'react-native-size-matters';
 
 const styles = ScaledSheet.create({
   TitleWrapper: {
-      marginHorizontal: '25@s',
-      marginTop: '30@s',
-      marginBottom: '10@s',
+    marginHorizontal: '25@s',
+    marginTop: '30@s',
+    marginBottom: '10@s',
   },
 
   Title: {
     textAlign: 'right',
     fontSize: '23@s',
+  },
+  Container: {
+    padding: '5@s',
+    marginVertical: '10@vs',
+    marginHorizontal: '20@s',
+    backgroundColor: '#fff',
+    borderRadius: '7@s',
+  },
+  Wrapper: {
+    alignItems: 'center',
+  },
+  Image: {
+    width: 345,
+    height: 256,
+  },
+  ImageWrapper: {
+    marginTop: '-10@s',
+  },
+  ContentWrapper: {
+    flexDirection: 'row',
+    marginTop: '-15@s',
+    direction: 'rtl',
+    alignSelf: 'stretch',
+    alignContent: 'flex-start',
+  },
+  TextWrapper: {
+    marginHorizontal: '15@s',
+    alignItems: 'flex-start',
+    marginRight: '10@s',
+    maxWidth: 260,
+    width: 260,
+  },
+  MainTitle: {
+    fontSize: '17@s',
+    marginBottom: '7@s',
+    textAlign: 'right',
+    direction: 'ltr',
+  },
+  SecTitle: {
+    color: '#222323',
+    fontSize: '15@s',
+    marginBottom: '10@s',
+    textAlign: 'right',
+    direction: 'ltr',
+  },
+
+  Icon: {
+    fontSize: '25@s',
+    backgroundColor: '#ED9F0B',
+    color: '#fff',
+    padding: '7@s',
+    overflow: 'hidden',
+    borderRadius: '21@s',
+  },
+  DescriptionWrapper: {
+    padding: '10@s',
+    marginHorizontal: '10@s',
+    alignItems: 'flex-end',
+  },
+  Description: {
+    color: '#222323',
+    fontSize: '12@s',
+    lineHeight: 20,
+    textAlign: 'right',
+    opacity: 0.5,
+    direction: 'ltr',
   },
 });
 
