28ac1b50a5617a30ae7c38d88d209b2121ad271d
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-11-22 00:26:14 +0200

    Installed React Navigation

--- src/Navication/HomeTabs.js
+++ src/Navication/HomeTabs.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HomeStack} from './HomeStack';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tabs = createBottomTabNavigator();
 
 export function HomeTabs(props) {
   return (
     <Tabs.Navigator>
-      <Tabs.Screen name="Main" component={HomeStack} />
+      <Tabs.Screen name="Home" component={HomeStack} />
       <Tabs.Screen name="Classes" component={HomeStack} />
       <Tabs.Screen name="Dictionary" component={HomeStack} />
       <Tabs.Screen name="MyAccount" component={HomeStack} />
