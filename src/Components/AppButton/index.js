7316a421013924744e2cc55434eee0a1461eb865
Mohmaed Malek <mohamed.malek.msm@gmail.com>
2020-11-24 23:55:35 +0200

    Foxed App Button Issues

--- src/Components/AppButton/index.js
+++ src/Components/AppButton/index.js
@@ -1,46 +1,34 @@
 import React from 'react';
-import {Text, ActivityIndicator, View} from 'react-native';
+import {Text, ActivityIndicator, View, Pressable} from 'react-native';
 import {Colors} from '../../Ulitis/Colors';
 
-export class AppButton extends React.Component {
-  render() {
-    const {
-      title,
-      WrapperStyle,
-      titleStyle,
-      isLoading,
-      disabled,
-      onPress,
-      ...rest
-    } = this.props;
+export const AppButton = (props) => {
+  const {title, WrapperStyle, titleStyle, isLoading, disabled, onPress} = props;
 
-    return (
-      <View
-        {...rest}
-        style={[
-          {
-            backgroundColor: Colors.yellow,
-            borderColor: Colors.yellow,
-            borderRadius: 7,
-            borderWidth: 1,
-            padding: 10,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-          WrapperStyle,
-          disabled ? {opacity: 0.4} : null,
-          isLoading ? {paddingVertical: 17} : null,
-        ]}>
-        {isLoading ? (
-          <ActivityIndicator />
-        ) : (
-          <Text
-            style={[{color: '#fff', fontWeight: 'bold'}, titleStyle]}
-            onPress={disabled || isLoading ? null : onPress}>
-            {title}
-          </Text>
-        )}
-      </View>
-    );
-  }
-}
+  return (
+    <Pressable
+      style={[
+        {
+          backgroundColor: Colors.yellow,
+          borderColor: Colors.yellow,
+          borderRadius: 7,
+          borderWidth: 1,
+          padding: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        WrapperStyle,
+        disabled ? {opacity: 0.4} : null,
+        isLoading ? {paddingVertical: 17} : null,
+      ]}
+      onPress={disabled || isLoading ? null : onPress}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text style={[{color: '#fff', fontWeight: 'bold'}, titleStyle]}>
+          {title}
+        </Text>
+      )}
+    </Pressable>
+  );
+};
